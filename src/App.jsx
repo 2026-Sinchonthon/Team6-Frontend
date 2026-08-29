0a15adea1a533fc31354b2f1d6ddd1d4caf837ea
이정준 <jngjnl1812@gmail.com>
2026-08-29 14:30:55 +0900

    [FEAT] 학교 선택 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import PageContainer from "./components/PageContainer";
+import SafeAreaTop from "./components/SafeAreaTop";
+import SchoolSelectPage from "./pages/SchoolSelectPage";
 
 function App() {
   return (
     <div className="flex min-h-screen justify-center bg-[#F1F3F5]">
       <div className="flex h-screen w-full max-w-100.5 flex-col bg-white">
+        <SafeAreaTop />
+
         {/* Header */}
 
         <PageContainer>
+          <SchoolSelectPage />
         </PageContainer>
 
         {/* GNB */}
