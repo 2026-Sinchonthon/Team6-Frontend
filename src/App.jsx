89342685974ed13e6b19223bdf9ae75abf35fd91
TuttoShin <starprickle29@gmail.com>
2026-08-29 13:03:28 +0900

    feat: 레이아웃 페이지 컨테이너 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
+import PageContainer from "./components/PageContainer";
 
 function App() {
-
   return (
-    <>
+    <div className="flex min-h-screen justify-center bg-[#F1F3F5]">
+      <div className="flex h-screen w-full max-w-120 flex-col bg-white">
+        {/* Header */}
 
-    </>
+        <PageContainer>{/* 페이지 콘텐츠 */}</PageContainer>
+
+        {/* GNB */}
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
